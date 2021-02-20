0b807af37f5651f8fb87a1014136278cda82eafb
Johnny Goth <Jgoth23@gmail.com>
2021-02-20 12:07:56 -0600

    like button created and counting

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import './App.css';
+import{useState} from 'react';
 
 function App() {
-    return (
+  //likeCount tracks my state
+  //setLikeCount lets me change the value of state
+    const [likeCount, setLikeCount] = useState(0);
+  return (
+    
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Here it is!</p>
         <img src="images/goat_small.jpg"/>
+        <div>
+        <p>Your photo has this many likes! {likeCount}</p>
+          <button onClick={() => setLikeCount(likeCount + 1)}>
+            Love this!
+          </button>
+          
+        </div>
+      
       </div>
+      
     );
 }
 
