2ec8931b67f93ec8a4f4fe97b753e9907a3ed966
Johnny Goth <Jgoth23@gmail.com>
2021-02-21 13:11:26 -0600

    button click working

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,12 +1,43 @@
+import Axios from "axios";
+import{useState} from 'react';
+
+
 function GalleryItem({galleryItem}){
+  const [likes, setLikes] = useState(0);
+  
+  
+  function addLike(evt) {
+    evt.preventDefault();
+
+    Axios.put({
+      method: 'PUT',
+      url: `/gallery/likes/${galleryItem.id}`,
+      data: likes
+    })
+    .then(() => {
+      console.log('put on');
+      fetchGallery();
+    })
+    .catch((error) => {
+      alert('error in put', error);
+    });
+    
+
+
+  }
 
 
 return (
   <>
   <img src={galleryItem.path} />
   {galleryItem.description}
-  </>
+  <p>You're image has: {likes}</p>
+  <button onClick={() => setLikes(likes + 1)}>
+  Likes!
+</button>
+</>
 
 )
+
 }
 export default GalleryItem;
